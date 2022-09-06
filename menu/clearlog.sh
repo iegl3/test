239dcc352647f6c7cd184e2fbafe837ec0d939d1
ⅰegle <51818383+iegl3@users.noreply.github.com>
2022-09-06 15:30:06 +0700

    Add files via upload

---
+++ menu/clearlog.sh
@@ -0,0 +1,10 @@
+echo > /etc/wildydev21/xray-mini-nontls/access.log
+echo > /etc/wildydev21/xray-mini-nontls/error.log
+echo > /etc/wildydev21/xray-mini-tls/access.log
+echo > /etc/wildydev21/xray-mini-tls/error.log
+echo > /etc/wildydev21/xray-mini-socks/access.log
+echo > /etc/wildydev21/xray-mini-socks/error.log
+echo > /etc/wildydev21/xray-mini-shadowsocks/access.log
+echo > /etc/wildydev21/xray-mini-shadowsocks/error.log
+echo > /var/log/syslog
+echo "$(date) | Successfull Clear Logs" > /etc/wildydev21/clearlog.log
